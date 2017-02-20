f5975e643a0baeb54e14ee8750e81d63ff7ec62f
soekmego <soekmen.goekhan@gmail.com>
2017-02-20 13:37:50 +0100

    added a few lines for you to learn, updated the readme file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # test1234
+
+testrepo for educational purposes.
+we are learning basic git commands and workflow over linux bash.
+if you are interested in collaboration send us a pm.
--- script.py
+++ script.py
@@ -1,5 +1,27 @@
+#
+#This script is for eduactional purposes
+#
+
 def hello(a):
     for n in range(a):
         print ("hello world")
 
 hello(300)
+
+#there is another way to spam ones console
+#by multiplying the value of the print statement:
+
+print ("hello world " * 300)
+
+#alternative way for a loop
+#this loop asks your to enter "your name"
+#if any other input is given, the loop continues to ask you to
+#enter "your name"
+
+def world():
+    while True:
+        print ("Please enter your name: ")
+        name = input()
+        if name == "your name":
+            break
+    print ("thank you")
